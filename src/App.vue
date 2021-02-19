e5447e52c4b41f122c795027d5bb528cd02aa456
Riccardo Polacci <rpolacci@groupon.com>
2021-02-19 10:49:06 +0100

    - Format code

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,25 @@
 <template>
-  <todo-list list-name="My to-dos" />
+    <todo-list list-name="My TO-DO list" />
 </template>
 
 <script>
-import TodoList from "./components/TodoList.vue";
+    import TodoList from './components/TodoList.vue'
 
-export default {
-  name: "App",
-  components: {
-    TodoList,
-  },
-};
+    export default {
+        name: 'App',
+        components: {
+            TodoList,
+        },
+    }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
